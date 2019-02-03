0136d768c1d665b7121a1808dccf104ff4b4da1f
javaking2013 <patrick.r.meadows@gmail.com>
2019-02-02 21:26:02 -0600

    Created framelss window

---
+++ src/main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron')
+
+let win
+
+app.on('ready', _ => {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        frame: false
+    })
+
+    win.loadFile('src/index.html')
+    win.on('closed', _ => {
+        win = null
+    })
+
+    //win.webContents.openDevTools()
+})
+
+app.on('window-all-closed', _ => {
+    app.quit()
+})
